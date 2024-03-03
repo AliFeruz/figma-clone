344019729efe5fd04556281f83f01ac0c31df416
Ali Feruz <147985313+AliFeruz@users.noreply.github.com>
2024-03-03 18:28:18 +0100

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import Live from "@/components/Live";
+import NavBar from "@/components/NavBar";
 
 
 export default function Page() {
   return (
     <div>
+    <NavBar/>
     <Live/>
     </div>
   );
